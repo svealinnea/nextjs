925bfc67689ae657432648e071933df597bf44b1
svea <svea@flexcharging.com>
2023-11-08 14:39:11 -0800

    removes prettier formatting

--- app/question5/page.tsx
+++ app/question5/page.tsx
@@ -2,18 +2,10 @@ export default function Page() {
   return (
     <div className="container mx-auto py-12">
       <h1 className="text-2xl font-semibold mb-4">
-        Imagine that the customer from Question4 responds with the following
-        email. Im so frustrated. Ive been trying to make this work for hours
-        and I just cant figure it out. It must be a platform issue so why don't
-        you just fix it for me instead of asking me questions. Write a reply to
-        the customer addressing the concerns raised.
+        Imagine that the customer from Question4 responds with the following email. Im so frustrated. Ive been trying to make this work for hours and I just cant figure it out. It must be a platform issue so why don't you just fix it for me instead of asking me questions. Write a reply to the customer addressing the concerns raised.
       </h1>
       <p className="text-sm opacity-50">
-        Hello, I am sorry to hear that you are frustrated, I understand. I would
-        be happy to help you with this issue, would you like to hop on a call or
-        video chat and we can walk through the process together? I am happy to
-        schedule some time to work through this with you. Looking forward to
-        your response, Svea
+        Hello, I am sorry to hear that you are frustrated, I understand. I would be happy to help you with this issue, would you like to hop on a call or video chat and we can walk through the process together? I am happy to schedule some time to work through this with you. Looking forward to your response, Svea
       </p>
     </div>
   );
