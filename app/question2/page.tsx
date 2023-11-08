a73d7eeb707d9186ae3203cb3b6bb429a667b9a6
svea <svea@flexcharging.com>
2023-11-08 12:46:22 -0800

    question 2 answers

---
+++ app/question2/page.tsx
@@ -0,0 +1,14 @@
+export default function Page() {
+  return (
+    <div className="container mx-auto py-12">
+      <h1 className="text-2xl font-semibold mb-4">
+      How would you compare Next.js with another framework? Feel free to compare with a framework of your choice
+      </h1>
+      <p className="text-sm opacity-50">
+        Next.js to me is similar to <a href="https://www.gatsbyjs.com/">Gatsby</a>. I used gatsby a bit when I was wanting to make a professional website for myself. I found that gatsby was great for static sites but when I wanted to add more dynamic content it was a bit of a pain. It seems like next.js is a bit more flexible in that regard. 
+        I have also been impressed with next.js in that it is very easy to get started with. I have been able to get a site up and running in a matter of minutes and the documentation is very easy to follow. Another thing that next.js seems to be better at than gatsby is the ability to use typescript. I have been using typescript for a while now and I love it.
+        I think for my professional website I will pivot and actually use next.js instead of gatsby. 
+      </p>
+    </div>
+  )
+}
