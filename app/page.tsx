030bb0205a3001314adbfecc9d60a43aee8f5016
svea <svea@flexcharging.com>
2023-11-08 13:59:21 -0800

    question 4 answer

--- app/page.tsx
+++ app/page.tsx
@@ -95,19 +95,70 @@ export default function Home() {
         </a>
 
         <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="/question4"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
+            Question 4{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            Answers the fourth question in the interview process.
+          </p>
+        </a>
+
+        <a
+          href="/question5"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Question 5{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Answers the fifth question in the interview process.
+          </p>
+        </a>
+
+        <a
+          href="/question6"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Question 6{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Answers the sixth question in the interview process.
+          </p>
+        </a>
+        
+        <a
+          href="/question7"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Question 7{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Answers the seventh question in the interview process.
           </p>
         </a>
       </div>
---
+++ app/question4/page.tsx
@@ -0,0 +1,24 @@
+export default function Page() {
+  return (
+    <div className="container mx-auto py-12">
+      <h1 className="text-2xl font-semibold mb-4">
+      A customer has a project on Vercel and they want to redirect the /blog path to another website. Write a reply to the customer.
+      </h1>
+      <p className="text-sm opacity-50">
+        Hello, 
+
+        I would be happy to assist you with that. 
+
+        I am curious what steps you have taken so far to try and redirect the /blog path to another website?
+
+        If there is something specific in your workflow that is not working I would be happy to help you troubleshoot that.
+
+        If you are just looking for information on how to do that I am happy to send that information over to you.
+
+        Looking forward to your response, 
+
+        Svea
+      </p>
+    </div>
+  )
+}
