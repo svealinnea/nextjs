217ac53f73fabfd73ef1b05918faef5d48c98573
svea <svea@flexcharging.com>
2023-11-08 14:22:45 -0800

    question 7

---
+++ app/question7/page.tsx
@@ -0,0 +1,14 @@
+export default function Page() {
+  return (
+    <div className="container mx-auto py-12">
+      <h1 className="text-2xl font-semibold mb-4">
+        How could we improve or alter this familiarization exercise?
+      </h1>
+      <p className="text-sm opacity-50">
+        To be honest, I really enjoyed this exercise. I think it was a great way
+        to learn more about Vercel and the product. Getting to play around and
+        research some of the capabilities was also really fun for me.
+      </p>
+    </div>
+  );
+}
